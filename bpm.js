bfcf8d5e2cc1317b3d3c86a0cdb4f88dfe12ac4f
GabrielEdJansen <93282987+GabrielEdJansen@users.noreply.github.com>
2021-10-27 16:27:12 -0300

    Update bpm.js

--- bpm.js
+++ bpm.js
@@ -1,3 +1,25 @@
+function inserirLinhaTabela() {
+
+  // Captura a referência da tabela com id “minhaTabela”
+  var table = document.getElementById("minhaTabela");
+  // Captura a quantidade de linhas já existentes na tabela
+  var numOfRows = table.rows.length;
+  // Captura a quantidade de colunas da última linha da tabela
+  var numOfCols = table.rows[numOfRows-1].cells.length;
+
+  // Insere uma linha no fim da tabela.
+  var newRow = table.insertRow(numOfRows);
+
+  // Faz um loop para criar as colunas
+  for (var j = 0; j < numOfCols; j++) {
+      // Insere uma coluna na nova linha 
+      newCell = newRow.insertCell(j);
+      // Insere um conteúdo na coluna
+      newCell.innerHTML = "Valor da Parcela " + numOfRows;
+  }
+
+}
+
 //Inicialização da API do workflow
 this.workflowCockpit = workflowCockpit({
   init: _init,
